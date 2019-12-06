21fdfe94a4d83398b83f3800e17dbc700ab3bfd6
OttoJHTX <sejeotto@gmail.com>
2019-12-06 12:55:12 +0100

    diddly done

--- piano.py
+++ piano.py
@@ -1,6 +1,5 @@
 import arcade
 import random
-from playsound import playsound
 
 width, height = 800, 600
 
@@ -10,6 +9,7 @@ class Innovation(arcade.Window):
         self.points = 0
         self.key = 0
         self.randomKey()
+        self.mode = "challenge"
 
     def update(self, delta_time):
         pass
@@ -17,23 +17,37 @@ class Innovation(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text(str(self.points) + " pts", width / 2, height / 3, arcade.color.WHITE, 15)
-        arcade.draw_text("Press " + str(self.keychoice), width / 2, height / 2, arcade.color.WHITE, 20)
+        if self.mode == "challenge":
+            arcade.draw_text("Press " + str(self.keychoice), width / 2, height / 2, arcade.color.WHITE, 20)
+        arcade.draw_text("Press 'X' to go out of {}".format(self.mode), width / 2, height / 3.5, arcade.color.WHITE, 20)
 
     def on_key_press(self, key, modifiers):
-        for x in range(0, len(self.keysButton)):
-            if (key == self.keysButton[x]):
-                if self.keychoice == self.keys[x]:
-                    playsound(self.sounds[x])
-                    self.points += 1
-                    self.randomKey()
+        if self.mode == "challenge":
+            for x in range(0, len(self.keysButton)):
+                if (key == self.keysButton[x]):
+                    if self.keychoice == self.keys[x]:
+                        sound = arcade.load_sound(self.sounds[x])
+                        arcade.play_sound(sound)
+                        self.points += 1
+                        self.randomKey()
+        if (key == arcade.key.X):
+            if self.mode == "challenge":
+                self.mode = "free mode"
+            elif self.mode == "free mode":
+                self.mode = "challenge"
+
+        if self.mode == "free mode":
+            for x in range(0, len(self.keysButton)):
+                if (key == self.keysButton[x]):
+                    sound = arcade.load_sound(self.sounds[x])
+                    arcade.play_sound(sound)
 
     def randomKey(self):
-        self.keys = ["C", "D", "E", "F", "G", "A", "B", "C#", "D#", "F#", "G#", "A#"]
-        self.keysButton = [arcade.key.UP, arcade.key.DOWN, arcade.key.LEFT, arcade.key.RIGHT, arcade.key.SPACE, arcade.key.W, arcade.key.A, arcade.key.S, arcade.key.D, arcade.key.F, arcade.key.G]
-        self.sounds = ["sounds/s1.mp3", "sounds/s2.wav", "sounds/s3.mp3", "sounds/s4.mp3", "sounds/s5.mp3", "sounds/s6.wav", "sounds/s7.wav", "sounds/s8.mp3", "sounds/s9.wav", "sounds/s10.mp3", "sounds/s11.mp3", "sounds/s12.ogg", ]
+        self.keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+        self.keysButton = [arcade.key.UP, arcade.key.RIGHT, arcade.key.DOWN, arcade.key.LEFT, arcade.key.SPACE, arcade.key.W, arcade.key.A, arcade.key.S, arcade.key.D, arcade.key.F, arcade.key.G, arcade.key.R]
+        self.sounds = ["sounds/s1.wav", "sounds/s2.wav", "sounds/s3.wav", "sounds/s4.wav", "sounds/s5.wav", "sounds/s6.wav", "sounds/s7.wav", "sounds/s8.wav", "sounds/s9.wav", "sounds/s10.wav", "sounds/s11.wav", "sounds/s12.wav", ]
         self.keychoice = random.choice(self.keys)
 
 
 mitSpil = Innovation(width, height)
-
 arcade.run()
